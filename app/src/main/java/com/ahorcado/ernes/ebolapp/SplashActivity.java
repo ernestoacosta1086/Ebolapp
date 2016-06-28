af3466afe3b07e483e863ccc1ba1bfc6aad2e374
Ernesto Acosta <ernesto.acosta1086@gmail.com>
2016-06-28 14:21:07 -0500

    Splash con otra activity

--- app/src/main/java/com/ahorcado/ernes/ebolapp/SplashActivity.java
+++ app/src/main/java/com/ahorcado/ernes/ebolapp/SplashActivity.java
@@ -2,12 +2,15 @@ package com.ahorcado.ernes.ebolapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
+
+
     }
 }
