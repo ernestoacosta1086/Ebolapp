6abf328216ffde68d68f03982b2f7dd245a10ecc
Ernesto Acosta <ernesto.acosta1086@gmail.com>
2016-06-29 12:55:39 -0500

    First Fragment

--- app/src/main/java/com/ahorcado/ernes/ebolapp/SplashActivity.java
+++ app/src/main/java/com/ahorcado/ernes/ebolapp/SplashActivity.java
@@ -1,5 +1,7 @@
 package com.ahorcado.ernes.ebolapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -11,6 +13,28 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+//        Runnable executeAfter3Seconds = new Runnable() {
+//            @Override
+//            public void run() {
+//                nextActivity();
+//                finish();
+//            }
+//        };
+//
+//        Handler handler = new Handler();
+//        handler.postDelayed(executeAfter3Seconds, 2500);
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                nextActivity();
+                finish();
+            }
+        }, 2500);
+    }
+
+    public void nextActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
